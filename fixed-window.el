288aa29a479dff337d7ef6249552ffe1724fc1b4
Taiju Aoki <aokitaiju0513@gmail.com>
2019-03-31 22:40:11 +0900

    update with comment

--- fixed-window.el
+++ fixed-window.el
@@ -1,9 +1,57 @@
-(defvar fixed-window nil)
-(defvar fixed-window-to-go t)
-(defvar fixed-window-truncate t)
-(defvar fixed-window-dedicated t)
+;;; fixed-window --- open file with a fixed width window
 
-(defun delete-fixed-window-if-needed ()
+;; Copyright (C) 2019- blue0513
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+;; Author: blue0513
+;; URL: https://github.com/blue0513/fixed-window
+;; Version: 0.1.0
+
+;;; Commentary:
+
+;; Edit your init.el
+;;
+;; (require 'fixed-window)
+;;
+
+;;; Code:
+
+(defvar fixed-window nil)
+
+(defcustom fixed-window-disable-switch t
+  "Disable to switch to the fixed-window by `other-window'."
+  :type 'boolean
+  :group 'fixed-window)
+
+(defcustom fixed-window-disable-truncate nil
+  "Disable to truncate in the fixed-window."
+  :type 'boolean
+  :group 'fixed-window)
+
+(defcustom fixed-window-dedicated t
+  "Make fixed-window dedicated."
+  :type 'boolean
+  :group 'fixed-window)
+
+(defcustom fixed-window-width-ratio 0.85
+  "Width ratio of fixed-window."
+  :type 'float
+  :group 'fixed-window)
+
+(defun fw--delete-fixed-window-if-needed ()
   (if (and
        (member fixed-window (window-list))
        (windowp fixed-window))
@@ -11,20 +59,20 @@
 	(delete-window fixed-window)
 	(setq fixed-window nil))))
 
-(defun open-fixed-window-with-file (buffer-name filename)
+(defun fw--open-fixed-window-with-file (buffer-name filename)
   (with-current-buffer buffer-name
     (find-file filename)
-    (set-window-parameter fixed-window 'no-other-window (not fixed-window-to-go))
+    (set-window-parameter fixed-window 'no-other-window fixed-window-disable-switch)
     (set-window-dedicated-p fixed-window fixed-window-dedicated)
-    (setq truncate-partial-width-windows (not fixed-window-truncate))
+    (setq truncate-partial-width-windows fixed-window-disable-truncate)
     (setq window-size-fixed 'width)
     (setq fixed-window (selected-window))))
 
-(defun fixed-window-validations (filename ratio)
+(defun fw--validations (filename ratio)
   (unless (file-exists-p filename) (error "No valid file found"))
   (if (and ratio (numberp ratio)) (error "Ratio must be number")))
 
-(defun goto-fixed-window ()
+(defun fixed-window-goto ()
   (interactive)
   ;; Hack
   (set-window-parameter fixed-window 'no-other-window nil)
@@ -32,22 +80,24 @@
   (other-window -1)
   (set-window-parameter fixed-window 'no-other-window t))
 
-(defun delete-fixed-window ()
+(defun fixed-window-delete ()
   (interactive)
-  (delete-fixed-window-if-needed))
+  (fw--delete-fixed-window-if-needed))
 
-(defun hide-fixed-window ()
-  (interactive)
-  (message "TODO"))
-
-(defun create-fixed-window (filename &optional ratio)
+(defun fixed-window-create (filename &optional ratio)
   (interactive "f")
-  (fixed-window-validations filename ratio)
-  (delete-fixed-window-if-needed)
-  (let* ((default-ratio 0.85)
+  (fw--validations filename ratio)
+  (fw--delete-fixed-window-if-needed)
+  (let* ((default-ratio fixed-window-width-ratio)
 	 (buffer-name " *temp-fixed-width-buffer")
 	 (width-ratio (if (null ratio) default-ratio ratio))
 	 (width (round (* (window-width) width-ratio))))
     (split-window-horizontally width)
     (switch-to-buffer (get-buffer-create buffer-name) t t)
-    (open-fixed-window-with-file buffer-name filename)))
+    (fw--open-fixed-window-with-file buffer-name filename)))
+
+;; * provide
+
+(provide 'fixed-window)
+
+;;; fixed-window.el ends here
